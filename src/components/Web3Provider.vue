f2f56eea81e862f930e327f714530f40d5b00cb3
Adrien Hobbs <adrien.hobbs@gmail.com>
2023-01-25 16:51:02 -0500

    add web3provider component

---
+++ src/components/Web3Provider.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { useProvider } from "../composables/useProvider";
+const { hasProvider, hasInit, error } = useProvider();
+</script>
+
+<template>
+  <slot v-if="hasProvider && hasInit" name="hasWeb3" />
+  <slot v-if="hasInit && !hasProvider" name="noWeb3">
+    {{ error }}
+  </slot>
+</template>
+
+<style scoped></style>
