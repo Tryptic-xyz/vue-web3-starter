92cfcc26dabb8b3242efbdea35750a41193cd546
Adrien Hobbs <adrien.hobbs@gmail.com>
2023-03-07 12:02:25 -0500

    add todo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import ConnectWalletButton from "../components/ConnectWalletButton.vue";
+// import LoadingButton from "../components/LoadingButton.vue";
 </script>
 
 <template>
   <main>
     <ConnectWalletButton />
+    <!-- <LoadingButton btnText="Click me" :loading="true" /> -->
   </main>
 </template>
