17298a2a9dae1a164a60bc030099a4fb4fd11deb
Adrien Hobbs <adrien.hobbs@gmail.com>
2023-03-09 15:31:32 -0500

    add network to useprovider

--- src/composables/useProvider.js
+++ src/composables/useProvider.js
@@ -1,39 +1,49 @@
 import detectEthereumProvider from "@metamask/detect-provider";
 import { ref, watch } from "vue";
 
-const goerliProviderURL =
-  "https://eth-goerli.g.alchemy.com/v2/D_wvYxbSIbPs3n7F44pv-zahs5Du-ti4";
+import { useConnectedNetwork } from "./useConnectedNetwork";
 
-const mainNetProviderURL =
-  "https://eth-mainnet.g.alchemy.com/v2/_kk3D76yzBoH7SoPR-iKvpDd9cIDAqFi";
+const providers = {
+  mainnet:
+    "https://eth-mainnet.g.alchemy.com/v2/_kk3D76yzBoH7SoPR-iKvpDd9cIDAqFi",
+  goerli:
+    "https://eth-goerli.g.alchemy.com/v2/D_wvYxbSIbPs3n7F44pv-zahs5Du-ti4",
+};
 
 const error = ref(false);
 const provider = ref(null);
 const goerliProvider = ref(null);
 const hasProvider = ref(false);
 const hasInit = ref(false);
+const { network, getNetwork, onNetworkChanged } = useConnectedNetwork();
 
 export const useProvider = () => {
   (async () => {
     if (!hasProvider.value && !hasInit.value) {
       hasProvider.value = await detectEthereumProvider();
-      hasInit.value = true;
 
       if (hasProvider.value) {
-        provider.value =
-          window.AlchemyWeb3.createAlchemyWeb3(mainNetProviderURL);
-        goerliProvider.value =
-          window.AlchemyWeb3.createAlchemyWeb3(goerliProviderURL);
+        await getNetwork();
+        provider.value = window.AlchemyWeb3.createAlchemyWeb3(
+          providers[network.name]
+        );
+        hasInit.value = true;
       } else {
         error.value = "Please visit this website from a web3 enabled browser.";
       }
     }
   })();
 
+  onNetworkChanged((networkName) => {
+    provider.value = window.AlchemyWeb3.createAlchemyWeb3(
+      providers[networkName]
+    );
+  });
+
   const onProviderInit = (onInit) => {
     watch(
       () => provider.value,
-      () => {
+      async () => {
         if (provider.value) {
           onInit();
         }
@@ -49,5 +59,6 @@ export const useProvider = () => {
     provider,
     error,
     hasInit,
+    network,
   };
 };
