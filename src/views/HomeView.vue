cb745e123af6985c23fa1e20650c3dee126ae49c
Adrien Hobbs <adrien.hobbs@gmail.com>
2023-03-09 15:58:15 -0500

    add pkgs

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,26 @@
 <script setup>
 import NumberCounter from "../components/NumberCounter.vue";
+import LoadingButton from "../components/LoadingButton.vue";
+import { useTestContract } from "../composables/useTestContract";
+import { showTxToast } from "../utils/ToastComponents";
+
+const {
+  mintObservers,
+  hasSniperPass,
+  hasPurveyorPass,
+  txPending,
+  hasCommittedPurveyorPass,
+  hasInit,
+  observerBalance,
+  redeemObservers,
+} = useTestContract();
+
+// const {onTxError, onTxSuccess, write} = test
+const m = async (q) => {
+  const tx = await mintObservers(q);
+  console.log(tx);
+  showTxToast(tx);
+};
 </script>
 
 <template>
@@ -7,5 +28,11 @@ import NumberCounter from "../components/NumberCounter.vue";
     <div class="flex items-center justify-center">
       <NumberCounter />
     </div>
+
+    <LoadingButton
+      :loading="txPending"
+      @click="() => m('7')"
+      btnText="click me"
+    />
   </main>
 </template>
